d86ecb988d0d25d4210000254572616175358e1b
MylesMorrs <mylesdavisjunior@gmail.com>
2025-07-19 16:14:07 -0600

    Fixed Subscriber.py

--- scripts/Subscriber.py
+++ scripts/Subscriber.py
@@ -1,20 +1,17 @@
 import paho.mqtt.client as mqtt
 
-ipAdress = input("Input IP Adress Of Mother: ")
+ip_address = input("Input IP Address Of Mother (broker): ")
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code", rc)
+def on_connect(client, userdata, flags, reasonCode, properties):
+    print("Connected with reason code", reasonCode)
     client.subscribe("test/topic")
 
 def on_message(client, userdata, msg):
     print(f"Received message on {msg.topic}: {msg.payload.decode()}")
 
-client = mqtt.Client()
+client = mqtt.Client(protocol=mqtt.MQTTv5)
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Update callback API version
-client._callback_api_version = 5  
-
-client.connect('192.168.1.85', 1883, 60)
+client.connect(ip_address, 1883, 60)
 client.loop_forever()
