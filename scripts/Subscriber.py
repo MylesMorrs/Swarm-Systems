cbc56bc34bbd3a0f86b5219a223074a43d0a0085
MylesMorrs <mylesdavisjunior@gmail.com>
2025-07-19 15:48:38 -0600

    Update Subscriber.py

--- scripts/Subscriber.py
+++ scripts/Subscriber.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 
+ipAdress = input("Input IP Adress Of Mother: ")
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code", rc)
     client.subscribe("test/topic")
@@ -11,5 +13,5 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(ipAdress, 1883, 60)
 client.loop_forever()
