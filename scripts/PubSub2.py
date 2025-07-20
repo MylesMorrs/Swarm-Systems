7d6df59ca20a5e9b536a610b09240b34c625eb47
Myles <mylesdavisjunior@gmail.com>
2025-07-19 21:03:49 -0600

    Create PubSub2.py

---
+++ scripts/PubSub2.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+import socket
+
+client = mqtt.Client(protocol=mqtt.MQTTv5)
+
+ip_address = "192.168.1.85"
+
+def on_connect(client, userdata, flags, reasonCode, properties):
+    print("Connected with reason code", reasonCode)
+    client.subscribe("1")
+
+def on_message(client, userdata, msg):
+    print(f"Received message on {msg.topic}: {msg.payload.decode()}")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(ip_address, 1883, 60)
+client.loop_start()  # Start the network loop in a background thread
+
+try:
+    while True:
+        client.publish("2", input("Enter message to publish: "))
+        # print("Message sent.")
+except KeyboardInterrupt:
+    print("Exiting...")
+    client.loop_stop()
+    client.disconnect()
