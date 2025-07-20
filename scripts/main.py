8d6cefb5e6b9b00a29f5052ed5afe87cf51434ce
RileyMorris05 <riley.morris@student.nmt.edu>
2025-07-19 20:30:43 -0700

    added reconnect for 60s

--- scripts/main.py
+++ scripts/main.py
@@ -1,39 +1,53 @@
 import paho.mqtt.client as mqtt
-import time
 import socket
+import time
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # Doesn't have to be reachable
-        s.connect(('10.255.255.255', 1))
-        ip = s.getsockname()[0]
-    except Exception:
-        ip = '127.0.0.1'
-    finally:
-        s.close()
-    return ip
-
-ip_address = get_ip_address()
-print(f"My IP address is: {ip_address}")
-
-# Use MQTTv5 for latest API
 client = mqtt.Client(protocol=mqtt.MQTTv5)
+
+ip_address = "192.168.1.85"
 
 def on_connect(client, userdata, flags, reasonCode, properties):
     print("Connected with reason code", reasonCode)
+    client.subscribe("2")
+
+
+RECONNECT_DELAY = 1
+MAX_RECONNECT_COUNT = 60
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+
+
+def on_message(client, userdata, msg):
+    print(f"Received message on {msg.topic}: {msg.payload.decode()}")
 
 client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(ip_address, 1883, 60)
 client.loop_start()  # Start the network loop in a background thread
 
 try:
     while True:
-        client.publish("test/topic", input("Enter message to publish: "))
+        client.publish("1", input("Enter message to publish: "))
         # print("Message sent.")
 except KeyboardInterrupt:
     print("Exiting...")
     client.loop_stop()
     client.disconnect()
-
